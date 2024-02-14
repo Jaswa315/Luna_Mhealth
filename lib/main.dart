512107acce67d199b3699ef02df0865b92d66f40
Shaun Stangler <shauns@uw.edu>
2024-02-14 11:24:27 -0800

    Configuration Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'views/home_page.dart';
 
-void main() {
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  await GlobalConfiguration().loadFromAsset("app_settings");
   runApp(MyApp());
 }
 
