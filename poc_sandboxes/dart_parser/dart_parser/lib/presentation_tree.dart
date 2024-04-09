92ae42e2abc1501bc76e75a464151ab11107aa90
Olivia Munineath Borath <135278387+oliviaborath@users.noreply.github.com>
2024-04-09 16:30:19 -0700

    use the new parse tree to map textbox to uid

---
+++ poc_sandboxes/dart_parser/dart_parser/lib/presentation_tree.dart
@@ -0,0 +1,208 @@
+enum ShapeGeometry { ellipse, rectangle, line }
+
+class Offset {
+  final double x;
+  final double y;
+
+  Offset(this.x, this.y);
+
+  Map<String, dynamic> toJson() {
+    return {'x': x, 'y': y};
+  }
+}
+
+class Size {
+  final double x;
+  final double y;
+
+  Size(this.x, this.y);
+
+  Map<String, dynamic> toJson() {
+    return {'x': x, 'y': y};
+  }
+}
+
+class PrsNode {
+  late String name;
+  List<PrsNode> children = [];
+
+  PrsNode();
+
+  Map<String, dynamic> toJson() {
+    return {
+      name: {'children': children.map((child) => child.toJson()).toList()}
+    };
+  }
+}
+
+class PresentationNode extends PrsNode {
+  late final String title;
+  late final String author;
+  late final int slideCount;
+
+  PresentationNode() {
+    name = 'presentation';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      name: {
+        'type': name,
+        'title': title,
+        'author': author,
+        'slideCount': slideCount,
+        'slides': children.map((child) => child.toJson()).toList()
+      }
+    };
+  }
+}
+
+class SlideNode extends PrsNode {
+  late final int slideNum;
+
+  SlideNode() {
+    name = 'slide';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'slideNum': slideNum,
+      'shapes': children.map((child) => child.toJson()).toList()
+    };
+  }
+}
+
+class TextBoxNode extends PrsNode {
+  // added for uid map to text box
+  static int _idCounter  = 0; // static counter
+  late final int uid;
+
+
+  TextBoxNode() {
+    name = 'textbox';
+    uid = _idCounter++; // assign and increment UID
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'uid': uid.toString().padLeft(3, '0'), // convert to string with leading 0s
+      
+      // todo, change children name to shape and textbody based on type
+      'children': children.map((child) => child.toJson()).toList()
+    };
+  }
+}
+
+class TextBodyNode extends PrsNode {
+  late final String? wrap;
+
+  TextBodyNode() {
+    name = 'textbody';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'wrap': wrap,
+      'paragraphs': children.map((child) => child.toJson()).toList()
+    };
+  }
+}
+
+class TextParagraphNode extends PrsNode {
+  late final String? alignment;
+
+  TextParagraphNode() {
+    name = 'paragraph';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'alignment': alignment,
+      'textgroups': children.map((child) => child.toJson()).toList()
+    };
+  }
+}
+
+class TextNode extends PrsNode {
+  late final bool italics;
+  late final bool bold;
+  late final bool underline;
+  late final int? size;
+  late final String? color;
+  late final String? highlightColor;
+  late final String? text;
+  TextNode() {
+    name = 'text';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'italics': italics,
+      'bold': bold,
+      'underline': underline,
+      'size': size,
+      'color': color,
+      'highlightcolor': highlightColor,
+      'text': text
+    };
+  }
+}
+
+class TitleNode extends PrsNode {
+  TitleNode() {
+    name = 'title';
+  }
+}
+
+class BodyNode extends PrsNode {
+  BodyNode() {
+    name = 'body';
+  }
+}
+
+class ShapeNode extends PrsNode {
+  late final Offset offset;
+  late final Size size;
+  late final ShapeGeometry shape;
+
+  ShapeNode(this.offset, this.size, this.shape) {
+    name = shape.name;
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {'type': name, "offset": offset, "size": size, "shape": shape.name};
+  }
+}
+
+class ImageNode extends PrsNode {
+  late final String? imageName;
+  late final String path;
+  late final String? altText;
+
+  ImageNode() {
+    name = 'image';
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'type': name,
+      'imagename': imageName,
+      'path': path,
+      'alttext': altText,
+      'shapes': children.map((child) => child.toJson()).toList()
+    };
+  }
+}
