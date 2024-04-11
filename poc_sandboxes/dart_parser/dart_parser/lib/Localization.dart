058d1bfbfd7bcceb7f61019f34489652338ed25f
alan.lai1738 <youremail@example.com>
2024-04-10 19:17:07 -0700

    minor commit, just printout of the map was added

--- poc_sandboxes/dart_parser/dart_parser/lib/Localization.dart
+++ poc_sandboxes/dart_parser/dart_parser/lib/Localization.dart
@@ -50,29 +50,44 @@ class Localization {
   void mapString(int id, String str) {
     stringMapping[id] = str; // Add mapping to string mapping
   }
+
+  /// Prints the string mapping.
+  void printMap() {
+    stringMapping.forEach((key, value) {
+      print('UID: $key, String: $value');
+    });
+  }
 }
 
 /// Main function to parse presentation, perform localization, and print tree as JSON.
 void main() {
-  var filename = "Luna_sample_module.pptx"; // Filename of the PowerPoint presentation
+  var filename =
+      "Luna_sample_module.pptx"; // Filename of the PowerPoint presentation
 
   File pptx = File(filename); // Open presentation file
 
-  PresentationParser parse = PresentationParser(pptx); // Create presentation parser
+  PresentationParser parse =
+      PresentationParser(pptx); // Create presentation parser
 
-  PrsNode prsTree = parse.parsePresentation(); // Parse presentation into a tree structure
+  PrsNode prsTree =
+      parse.parsePresentation(); // Parse presentation into a tree structure
 
   Localization localizer = Localization(); // Create localization object
 
   // Modifying prsTree UID directly (Pass and modify by reference)
-  localizer.walkTreeAndAssignUIDs(prsTree); // Assign UIDs to text nodes in the presentation tree
+  localizer.walkTreeAndAssignUIDs(
+      prsTree); // Assign UIDs to text nodes in the presentation tree
 
   printTreeAsJSON(prsTree); // Print the updated presentation tree as JSON
+
+  localizer.printMap();
 }
 
 /// Function to print the presentation tree as JSON to a file.
 void printTreeAsJSON(PrsNode prsTree) {
-  Map<String, dynamic> astJson = prsTree.toJson(); // Convert presentation tree to JSON
-  String jsonOutput = JsonEncoder.withIndent('  ').convert(astJson); // Encode JSON with indentation
+  Map<String, dynamic> astJson =
+      prsTree.toJson(); // Convert presentation tree to JSON
+  String jsonOutput = JsonEncoder.withIndent('  ')
+      .convert(astJson); // Encode JSON with indentation
   File('module.json').writeAsStringSync(jsonOutput); // Write JSON to file
 }
