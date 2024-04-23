694856ea5ee7fb638becc0936eae3692dd22760a
Olivia Munineath Borath <135278387+oliviaborath@users.noreply.github.com>
2024-04-23 15:46:23 -0700

    updated Localize file after resolving mc

--- luna_authoring_system/ppt_parser/lib/presentation_tree.dart
+++ luna_authoring_system/ppt_parser/lib/presentation_tree.dart
@@ -6,7 +6,6 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import 'uid_object.dart';
 
 enum ShapeGeometry { ellipse, rectangle, line }
 
