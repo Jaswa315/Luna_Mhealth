4202c0f8484d71616bf34b0ab15b9eb3a20c3535
anagar@uw.edu <anagar@uw.edu>
2024-04-29 14:50:04 -0700

    Refactor HomePage widget and update module loading logic

--- luna_mhealth_mobile/lib/presentation/pages/home_page.dart
+++ luna_mhealth_mobile/lib/presentation/pages/home_page.dart
@@ -8,11 +8,13 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:luna_mhealth_mobile/core/constants/constants.dart';
 import 'package:provider/provider.dart';
 
+import '../../core/constants/constants.dart';
+import '../../core/services/page_builder_service.dart';
+import '../../core/services/page_persistence_service.dart';
 import '../../models/module.dart';
-import '../../providers/click_state_provider.dart';
+import '../../providers/module_provider.dart';
 import 'module_page.dart';
 
 /// The home page of the application.
@@ -23,38 +25,58 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    print("HomePage initState");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("HomePage didChangeDependencies");
+  }
+
+  @override
+  void dispose() {
+    print("HomePage dispose");
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("HomePage build method called");
     return Scaffold(
       appBar: AppBar(
         title: const Text(AppConstants.appName),
         actions: [
           IconButton(
-            onPressed: () {
-              Provider.of<ClickStateProvider>(context, listen: false)
-                  .loadModules();
-            },
-            icon: const Icon(
-              CupertinoIcons.add_circled,
-            ),
+            onPressed: () => Provider.of<ModuleProvider>(context, listen: false)
+                .selectAndStoreModuleFile(),
+            icon: const Icon(CupertinoIcons.add_circled),
           ),
         ],
       ),
       body: Center(
-        child: Consumer<ClickStateProvider>(
-          builder: (context, clickStateProvider, child) {
-            if (clickStateProvider.modulesLoaded) {
+        child: Consumer<ModuleProvider>(
+          builder: (context, moduleProvider, child) {
+            if (moduleProvider.areModulesLoaded) {
               return ListView.builder(
-                itemCount: clickStateProvider.loadedModules.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(clickStateProvider.loadedModules[index].title),
-                    onTap: () => _navigateToModulePage(
-                        context, clickStateProvider.loadedModules[index]),
-                  );
-                },
+                itemCount: moduleProvider.moduleList.length,
+                itemBuilder: _buildModuleListItem,
               );
             } else {
-              return const Text(AppConstants.noModuleErrorMessage);
+              return FutureBuilder(
+                future: moduleProvider.loadAvailableModules(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return const Text('No modules available');
+                  }
+                },
+              );
             }
           },
         ),
@@ -63,11 +85,26 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+Widget _buildModuleListItem(BuildContext context, int index) {
+  final module =
+      Provider.of<ModuleProvider>(context, listen: false).moduleList[index];
+  return ListTile(
+    title: Text(module.title),
+    onTap: () => _navigateToModulePage(context, module),
+  );
+}
+
 void _navigateToModulePage(BuildContext context, Module module) {
+  ModulePageBuilderService pageServices = ModulePageBuilderService();
+  PagePersistenceService persistenceService = PagePersistenceService();
   Navigator.push(
     context,
     MaterialPageRoute(
-      builder: (context) => ModulePage(module: module),
+      builder: (context) => ModulePage(
+        module: module,
+        pageServices: pageServices,
+        persistenceService: persistenceService,
+      ),
     ),
   );
 }
--- luna_mhealth_mobile/lib/presentation/pages/module_page.dart
+++ luna_mhealth_mobile/lib/presentation/pages/module_page.dart
@@ -9,136 +9,129 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../models/image/image_component.dart';
+import '../../core/controllers/navigation_controller.dart';
+import '../../core/services/page_builder_service.dart';
+import '../../core/services/page_persistence_service.dart';
 import '../../models/module.dart';
 import '../../models/page.dart' as page_model;
-import '../../models/text/text_component.dart';
+import '../widgets/module_page_navigation.dart';
 
 /// A page that displays the content of a module.
 class ModulePage extends StatefulWidget {
   /// The module to display.
   final Module module;
 
-  /// Constructs a new [ModulePage] with the given [module].
-  ModulePage({Key? key, required this.module}) : super(key: key);
+  /// The services for managing module pages.
+  final ModulePageBuilderService pageServices;
+
+  /// The service for persisting module data.
+  final PagePersistenceService persistenceService;
+
+  /// Creates a [ModulePage] widget.
+  ModulePage({
+    Key? key,
+    required this.module,
+    required this.pageServices,
+    required this.persistenceService,
+  }) : super(key: key);
 
   @override
   State<ModulePage> createState() => _ModulePageState();
 }
 
 class _ModulePageState extends State<ModulePage> {
-  int _currentPage = 0;
-  final PageController _pageController = PageController();
+  late NavigationController _navigationController;
+  late List<Widget> _cachedPagesWidgets;
+
+  @override
+  void initState() {
+    print("ModulePage initState");
+    super.initState();
+    _navigationController =
+        NavigationController(pageController: PageController());
+    _loadLastVisitedPage();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("ModulePage didChangeDependencies");
+    _setupPageWidgets();
+  }
 
   @override
   void dispose() {
-    _pageController.dispose();
+    _saveLastVisitedPage();
+    _navigationController.pageController.dispose();
     super.dispose();
+    print("ModulePage dispose");
   }
 
   @override
   Widget build(BuildContext context) {
-    // Get screen size
-    final screenSize = MediaQuery.of(context).size;
+    print("ModulePage build method called");
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.module.title),
-        leading: IconButton(
-          icon: Icon(CupertinoIcons.back),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
+          title: Text(widget.module.title),
+          leading: IconButton(
+              icon: Icon(CupertinoIcons.back),
+              onPressed: () => Navigator.pop(context))),
       body: Column(
         children: [
           Expanded(
             child: PageView.builder(
-              controller: _pageController,
-              itemCount: widget.module.pages.length,
-              onPageChanged: (index) {
-                setState(() {
-                  _currentPage = index;
-                });
-              },
-              itemBuilder: (context, index) =>
-                  buildSlide(widget.module.pages[index], screenSize),
-            ),
+                controller: _navigationController.pageController,
+                itemCount: widget.module.pages.length,
+                onPageChanged: (index) {
+                  setState(() => _navigationController.currentPage = index);
+                  _saveLastVisitedPage();
+                },
+                itemBuilder: (context, index) => _cachedPagesWidgets[index]),
           ),
-          modulePageNavigation(),
+          ModulePageNavigation(
+            currentPage: _navigationController.currentPage,
+            pageCount: widget.module.pages.length,
+            onPageChange: (index) => _navigationController.animateToPage(index),
+          ),
         ],
       ),
     );
   }
 
-  Widget buildSlide(page_model.Page page, Size screenSize) {
-    double componentScale = screenSize.width / widget.module.width;
-    return SingleChildScrollView(
-      child: Container(
-        width: screenSize.width,
-        height: screenSize.height,
-        child: Stack(
-          children: page.components.map((component) {
-            if (component is TextComponent) {
-              return Positioned(
-                left: component.bounds.left * componentScale,
-                top: component.bounds.top * componentScale,
-                width: component.bounds.width * componentScale,
-                height: component.bounds.height * componentScale,
-                child: component.render(),
-              );
-            } else if (component is ImageComponent) {
-              return Positioned(
-                left: component.bounds.left * componentScale,
-                top: component.bounds.top * componentScale,
-                width: component.bounds.width * componentScale,
-                height: component.bounds.height * componentScale,
-                child: component.render(),
-              );
-            } else {
-              // Placeholder for other component types like ImageComponent
-              // This can be expanded later to include actual rendering logic for images
-              return Container();
-            }
-          }).toList(),
-        ),
-      ),
-    );
+  Future<void> _loadLastVisitedPage() async {
+    int lastPage = await widget.persistenceService
+        .loadLastVisitedPage(widget.module.title);
+    setState(() => _navigationController.currentPage = lastPage);
+    _navigationController.jumpToPage(lastPage);
   }
 
-  Widget modulePageNavigation() {
-    // Navigation logic remains unchanged
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          TextButton(
-            onPressed: _currentPage > 0
-                ? () {
-                    _pageController.previousPage(
-                        duration: Duration(milliseconds: 300),
-                        curve: Curves.easeInOut);
-                  }
-                : null,
-            child: Text('Pre'),
-          ),
-          TextButton(
-            onPressed: () {
-              _pageController.jumpToPage(0);
-            },
-            child: Text('Page 1'),
-          ),
-          TextButton(
-            onPressed: _currentPage < widget.module.pages.length - 1
-                ? () {
-                    _pageController.nextPage(
-                        duration: Duration(milliseconds: 300),
-                        curve: Curves.easeInOut);
-                  }
-                : null,
-            child: Text('Next'),
-          ),
-        ],
-      ),
-    );
+  Future<void> _saveLastVisitedPage() async {
+    await widget.persistenceService.saveLastVisitedPage(
+        widget.module.title, _navigationController.currentPage);
+  }
+
+  void _setupPageWidgets() {
+    print("ModulePage _setupPageWidgets called");
+    final screenSize = MediaQuery.of(context).size;
+    _cachedPagesWidgets = widget.module.pages
+        .asMap()
+        .map((index, page) =>
+            MapEntry(index, _getCachedOrBuildSlide(page, screenSize, index)))
+        .values
+        .toList();
+  }
+
+  /// Builds or retrieves a cached slide for the given [page].
+  Widget _getCachedOrBuildSlide(
+      page_model.Page page, Size screenSize, int pageIndex) {
+    return widget.pageServices.buildOrRetrieveCachedPage(
+          widget.module,
+          pageIndex,
+          screenSize,
+          (page, size, scale) => widget.pageServices
+              .buildPage(page, size, scale, widget.module.title),
+        ) ??
+        SizedBox();
   }
 }
