392272b55a5f342c6e3a2db136f3ca1d46380c74
anagar@uw.edu <anagar@uw.edu>
2024-05-13 23:00:29 -0700

    Refactor component renderers and factory classes

---
+++ luna_mhealth_mobile/lib/renderers/irenderer.dart
@@ -0,0 +1,20 @@
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import 'package:flutter/material.dart';
+
+/// An abstract class that defines the contract for a component renderer.
+abstract class IRenderer {
+  /// Renders the given [component] with the specified [scale].
+  ///
+  /// The [component] parameter represents the dynamic component to be rendered.
+  /// The [scale] parameter represents the scaling factor for the component.
+  ///
+  /// Returns a [Widget] representing the rendered component.
+  Widget renderComponent(dynamic component, double scale);
+}
