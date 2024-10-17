2a40eb604a9f2c139c7752fa30c1db4a6c42fd93
Alan Lai <alanlai@Alans-MacBook-Pro-3.local>
2024-10-17 16:07:21 -0700

    Deleting all generated platform files in mobile and core

--- luna_core/android/app/src/main/kotlin/com/example/luna_core/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.luna_core
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- luna_mobile/android/app/src/main/kotlin/com/example/luna_mhealth_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.luna_mhealth_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
