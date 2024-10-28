638aba021f68dc06dd0b28ba5e8a674c92be2b51
Victor Long <vlong91@uw.edu>
2024-10-28 21:59:51 +0000

    Merged PR 983: Add a mobile test and added testing mobile to pipeline
    This PR adds a simple test to the mobile portion of the app. This then allows for the pipeline to test the luna_mobile folder.
    
    The mock framework used in the test is based on how the testing is done for path_provider itself.
    https://github.com/flutter/plugins/blob/main/packages/path_provider/path_provider/test/path_provider_test.dart

---
+++ luna_mobile/test/core/common/cache_dir_test.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:luna_mobile/core/common/cache_dir.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+// use test junk folder
+const String testJunkPath = 'test/junk';
+
+class MockPathProviderPlatform extends Mock with MockPlatformInterfaceMixin implements PathProviderPlatform {
+  //Override getTemporaryPath so that getTemporaryDirectory works outside android/ios devices
+  @override
+  Future<String> getTemporaryPath() async {
+    return testJunkPath;
+  }
+}
+
+void main() {
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  setUp(() {
+    PathProviderPlatform.instance = MockPathProviderPlatform();
+  });
+  
+  group('Cache Dir Exists Tests', () {
+    setUp(() async {
+      await new Directory(testJunkPath).create();
+    });
+      
+    test('Cache Dir Has One File', () async {
+      await new File(testJunkPath + '/test_file.txt').create();
+      List<String> cacheContents = await listCacheDirContents();
+      expect(cacheContents.length,1);
+    });
+
+    test('Cache Dir Has Zero Files', () async {
+      List<String> cacheContents = await listCacheDirContents();
+      expect(cacheContents.length,0);
+    });
+
+    tearDown(() async {
+      // clear out all files in the test folder
+      await new Directory(testJunkPath).delete(recursive: true);
+    });
+
+  });
+
+}
