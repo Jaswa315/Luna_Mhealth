eb057a93540367b9843f15bc297fd41ff1675a5f
Andrew Nah <auspiciouswho47@gmail.com>
2024-12-12 09:50:16 -0800

    check emu values

--- luna_core/lib/utils/emu.dart
+++ luna_core/lib/utils/emu.dart
@@ -8,11 +8,22 @@
 
 /// EMU(English Metric Units) is a measurement in computer typography.
 /// These units are used to translate on-screen layouts to printed layouts for specified printer hardware.
+/// EMU units are always positive as descrived in the c-rex documentation.
 /// More information about EMU
+/// https://c-rex.net/samples/ooxml/e1/Part4/OOXML_P4_DOCX_sldSz_topic_ID0EULOGB.html
 /// https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-odraw/40cd0cf9-f038-4603-b790-252d93e3e8fd
+
+import 'package:luna_core/utils/logging.dart';
 
 class EMU {
   int value;
 
-  EMU(this.value);
+  EMU(this.value) {
+    if (value < 0) {
+      LogManager().logTrace(
+        'value must be a non-negative integer, but got $value',
+        LunaSeverityLevel.Warning,
+      );
+    }
+  }
 }
