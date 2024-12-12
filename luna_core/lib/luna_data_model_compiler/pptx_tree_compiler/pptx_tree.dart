60b123a6171e91031ad7fe4fd492fcd720f499b8
Andrew Nah <auspiciouswho47@gmail.com>
2024-12-12 09:10:05 -0800

    tree and emu

---
+++ luna_core/lib/luna_data_model_compiler/pptx_tree_compiler/pptx_tree.dart
@@ -0,0 +1,30 @@
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+library pptx;
+
+import 'package:built_value/built_value.dart';
+import 'package:luna_core/utils/emu.dart';
+
+part 'pptx_tree.g.dart';
+
+abstract class PptxTree implements Built<PptxTree, PptxTreeBuilder> {
+  EMU get width;
+  EMU get height;
+
+  PptxTree._();
+
+  factory PptxTree([updates(PptxTreeBuilder b)]) = _$PptxTree;
+
+  @BuiltValueHook(initializeBuilder: true)
+  static void _setDefaults(PptxTreeBuilder b) {
+    b
+      ..width = EMU(-1)
+      ..height = EMU(-1);
+  }
+}
