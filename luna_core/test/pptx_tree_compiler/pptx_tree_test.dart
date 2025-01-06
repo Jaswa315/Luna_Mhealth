5b0483bbd750b8f1878389a37e03f162e57a2c80
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-06 09:13:12 -0800

    clean

--- luna_core/test/pptx_tree_compiler/pptx_tree_test.dart
+++ luna_core/test/pptx_tree_compiler/pptx_tree_test.dart
@@ -39,22 +39,4 @@ void main() {
       expect(pptxTree.height, null);
     });
   });
-
-  group('Tests for EMU', () {
-    test('Argument error is thrown when input is negative', () async {
-      expect(() => EMU((b) => b..value = -1), throwsA(isArgumentError));
-    });
-
-    test('Value can be initialized with zero', () async {
-      EMU emu = EMU((b) => b..value = 0);
-
-      expect(emu.value, 0);
-    });
-
-    test('Value can be initialized with positive integer', () async {
-      EMU emu = EMU((b) => b..value = 1);
-
-      expect(emu.value, 1);
-    });
-  });
 }
