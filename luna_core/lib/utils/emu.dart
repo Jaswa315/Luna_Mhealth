de4828a285ae5828f865dc4183127a339442b254
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-08 10:17:39 -0800

    delete a blank line between the comment and the item being annotated

--- luna_core/lib/utils/emu.dart
+++ luna_core/lib/utils/emu.dart
@@ -8,7 +8,6 @@ part 'emu.g.dart';
 /// More information about EMU
 /// https://c-rex.net/samples/ooxml/e1/Part4/OOXML_P4_DOCX_sldSz_topic_ID0EULOGB.html
 /// https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-odraw/40cd0cf9-f038-4603-b790-252d93e3e8fd
-
 abstract class EMU implements Built<EMU, EMUBuilder> {
   int get value;
 
