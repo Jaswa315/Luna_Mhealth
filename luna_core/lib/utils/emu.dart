96c6e1a1945788d2bf7fe812ef4dd4db1ebebf5d
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-08 16:14:44 -0800

    update class annotation

--- luna_core/lib/utils/emu.dart
+++ luna_core/lib/utils/emu.dart
@@ -4,7 +4,7 @@ part 'emu.g.dart';
 
 /// EMU(English Metric Units) is a measurement in computer typography.
 /// These units are used to translate on-screen layouts to printed layouts for specified printer hardware.
-/// EMU units are always positive as descrived in the c-rex documentation.
+/// EMU units are non-negative integers as descrived in the c-rex documentation.
 /// More information about EMU
 /// https://c-rex.net/samples/ooxml/e1/Part4/OOXML_P4_DOCX_sldSz_topic_ID0EULOGB.html
 /// https://learn.microsoft.com/en-us/openspecs/office_file_formats/ms-odraw/40cd0cf9-f038-4603-b790-252d93e3e8fd
