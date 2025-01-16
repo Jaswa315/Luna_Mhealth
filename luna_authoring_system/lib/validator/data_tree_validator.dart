059b6e8dbe180683838ad221c8f536aeeb6108ae
Alan Lai <alanlai@alans-mbp-4.lan>
2025-01-16 15:03:31 -0800

    [Luna Authoring] Add Module Dimension Validator Check

--- luna_authoring_system/lib/validator/data_tree_validator.dart
+++ luna_authoring_system/lib/validator/data_tree_validator.dart
@@ -6,13 +6,29 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/// Validator class with helper functions to for Luna data tree values.
-class DataTreeValidator {
+/// The Data Tree Validator class' purpose is to 
+/// check if all the values in a pptx data tree are valid to be
+/// converted into a Luna module, with the intent to check for
+/// corner cases on data variables within the data tree.
+class DataTreeValidator { 
+  DataTreeValidator();
+
+  /// Checks if module width and height are valid values. 
+  /// 
+  /// Returns `true` if the module's dimensions are valid.
+  bool isDataTreeModuleDimensionsValid(double modulePageWidth, double modulePageHeight) {
+    if(modulePageWidth <= 0 || modulePageHeight <= 0) {
+      return false;
+    }
+    
+    return true;
+  }
+
   /// Checks if a component is within the specified module boundaries.
   ///
   /// Returns `true` if both component's bottom-right coordinates (`componentBottomRightX`, `componentBottomRightY`)
   /// are within the `modulePageWidth` and `modulePageHeight`.
-  static bool isComponentInBounds(double modulePageWidth, double modulePageHeight,
+  bool isComponentInBounds(double modulePageWidth, double modulePageHeight,
       double componentBottomRightX, double componentBottomRightY) {
     return componentBottomRightX <= modulePageWidth &&
         componentBottomRightY <= modulePageHeight;
