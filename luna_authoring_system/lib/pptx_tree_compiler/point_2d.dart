2849eb0040065db636e50c77f8ce363c1c5a4baa
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-17 01:09:36 -0800

    point2d

---
+++ luna_authoring_system/lib/pptx_tree_compiler/point_2d.dart
@@ -0,0 +1,14 @@
+import 'package:built_value/built_value.dart';
+
+part 'point_2d.g.dart';
+
+/// Point2D class is the representation of a point in 2-dimensional space.
+/// It is used to describe offset and size values of the shapes in the .pptx.
+abstract class Point2D implements Built<Point2D, Point2DBuilder> {
+  double get x;
+  double get y;
+
+  Point2D._();
+
+  factory Point2D([updates(Point2DBuilder b)]) = _$Point2D;
+}
---
+++ luna_authoring_system/lib/pptx_tree_compiler/point_2d.g.dart
@@ -0,0 +1,101 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'point_2d.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+class _$Point2D extends Point2D {
+  @override
+  final double x;
+  @override
+  final double y;
+
+  factory _$Point2D([void Function(Point2DBuilder)? updates]) =>
+      (new Point2DBuilder()..update(updates))._build();
+
+  _$Point2D._({required this.x, required this.y}) : super._() {
+    BuiltValueNullFieldError.checkNotNull(x, r'Point2D', 'x');
+    BuiltValueNullFieldError.checkNotNull(y, r'Point2D', 'y');
+  }
+
+  @override
+  Point2D rebuild(void Function(Point2DBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  Point2DBuilder toBuilder() => new Point2DBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is Point2D && x == other.x && y == other.y;
+  }
+
+  @override
+  int get hashCode {
+    var _$hash = 0;
+    _$hash = $jc(_$hash, x.hashCode);
+    _$hash = $jc(_$hash, y.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper(r'Point2D')
+          ..add('x', x)
+          ..add('y', y))
+        .toString();
+  }
+}
+
+class Point2DBuilder implements Builder<Point2D, Point2DBuilder> {
+  _$Point2D? _$v;
+
+  double? _x;
+  double? get x => _$this._x;
+  set x(double? x) => _$this._x = x;
+
+  double? _y;
+  double? get y => _$this._y;
+  set y(double? y) => _$this._y = y;
+
+  Point2DBuilder();
+
+  Point2DBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _x = $v.x;
+      _y = $v.y;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(Point2D other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$Point2D;
+  }
+
+  @override
+  void update(void Function(Point2DBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  Point2D build() => _build();
+
+  _$Point2D _build() {
+    final _$result = _$v ??
+        new _$Point2D._(
+            x: BuiltValueNullFieldError.checkNotNull(x, r'Point2D', 'x'),
+            y: BuiltValueNullFieldError.checkNotNull(y, r'Point2D', 'y'));
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
