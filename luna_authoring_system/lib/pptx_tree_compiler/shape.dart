670c2ca2f3c2015b09c26d1d56ef8184b1c3fc61
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-19 11:24:49 -0800

    Revert "shape class"
    This reverts commit 33a6ce7b3039c483b8abfd5dad220489940263f4.

--- luna_authoring_system/lib/pptx_tree_compiler/shape.dart
+++
@@ -1,9 +0,0 @@
-import 'package:luna_authoring_system/pptx_tree_compiler/shape_type.dart';
-import 'package:luna_authoring_system/pptx_tree_compiler/transform.dart';
-
-/// The shape is the abtract class of concrete picture,
-/// connection, and textbox classes.
-abstract class Shape {
-  ShapeType get type;
-  Transform? get transform;
-}
