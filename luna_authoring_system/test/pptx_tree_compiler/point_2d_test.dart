287b8e9692b997925f712c3370c5eaf3d0742829
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-19 11:48:47 -0800

    fix unit tests for Point2D

--- luna_authoring_system/test/pptx_tree_compiler/point_2d_test.dart
+++ luna_authoring_system/test/pptx_tree_compiler/point_2d_test.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:built_value/built_value.dart';
 import 'package:luna_authoring_system/pptx_tree_compiler/point_2d.dart';
+import 'package:luna_core/utils/emu.dart';
 
 void main() {
   group('Tests for Point2D', () {
-    test('BuiltValueNullFieldError is thrown when x is missing', () {
-      expect(() => Point2D((b) => b..y = 0.0),
-          throwsA(predicate((e) => e is BuiltValueNullFieldError)));
+    test('BuiltValueNestedFieldError is thrown when x is missing', () {
+      expect(
+          () => Point2D((pb) => pb..y = EMU((eb) => eb..value = 0).toBuilder()),
+          throwsA(predicate((e) => e is BuiltValueNestedFieldError)));
     });
 
-    test('BuiltValueNullFieldError is thrown when y is missing', () {
-      expect(() => Point2D((b) => b..x = 0.0),
-          throwsA(predicate((e) => e is BuiltValueNullFieldError)));
+    test('BuiltValueNestedFieldError is thrown when y is missing', () {
+      expect(() => Point2D((b) => b..x = EMU((b) => b..value = 0).toBuilder()),
+          throwsA(predicate((e) => e is BuiltValueNestedFieldError)));
     });
   });
 }
