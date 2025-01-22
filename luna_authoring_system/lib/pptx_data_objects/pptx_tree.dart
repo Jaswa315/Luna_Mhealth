abd787b8a32728d91215e765dff8e466418b8bca
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-21 20:34:22 -0800

    move pptx objects to different directory

--- luna_authoring_system/lib/pptx_data_objects/pptx_tree.dart
+++ luna_authoring_system/lib/pptx_data_objects/pptx_tree.dart
@@ -1,18 +1,16 @@
-import 'package:built_value/built_value.dart';
 import 'package:luna_core/utils/emu.dart';
-
-part 'pptx_tree.g.dart';
 
 /// The PptxTree represents the PowerPoint data as a tree.
 /// It stores every data we need to build a luna module,
 /// containing pptx meta data, slide master, slide layout, slide, and section data.
 /// For more information, check the domain diagram at this url.
 /// https://app.diagrams.net/#G1oO_dalj6fEq2QAfdHzyQPQiMiSi9CpXE#%7B%22pageId%22%3A%22JfLex2GAHqd8IyoLERXG%22%7D
-abstract class PptxTree implements Built<PptxTree, PptxTreeBuilder> {
-  EMU? get width;
-  EMU? get height;
+class PptxTree {
+  String? title;
+  String? author;
 
-  PptxTree._();
+  EMU? width;
+  EMU? height;
 
-  factory PptxTree([updates(PptxTreeBuilder b)]) = _$PptxTree;
+  PptxTree();
 }
