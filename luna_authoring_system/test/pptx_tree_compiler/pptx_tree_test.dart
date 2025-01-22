075638158290294f33f2942a5f58d331abc31932
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-21 20:34:39 -0800

    dependency fix

--- luna_authoring_system/test/pptx_tree_compiler/pptx_tree_test.dart
+++ luna_authoring_system/test/pptx_tree_compiler/pptx_tree_test.dart
@@ -1,16 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:luna_core/utils/logging.dart';
 import 'package:global_configuration/global_configuration.dart';
-import 'package:luna_authoring_system/luna_data_model_compiler/pptx_tree_compiler/pptx_tree.dart';
+import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_core/utils/emu.dart';
+import 'package:built_collection/built_collection.dart';
 
 void main() {
-  setUpAll(() async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    await GlobalConfiguration().loadFromAsset("app_settings");
-    await LogManager.createInstance();
-  });
-
   group('Tests for PptxTree', () {
     test('width is initialized with null value', () async {
       PptxTree pptxTree = PptxTree();
@@ -42,6 +37,36 @@ void main() {
           (b) => b..height = EMU((b) => b..value = height).toBuilder());
 
       expect(pptxTree.height?.value, height);
+    });
+
+    test('title is initialized with null value', () async {
+      PptxTree pptxTree = PptxTree();
+
+      expect(pptxTree.title, null);
+    });
+
+    test('title can be updated with a String value', () async {
+      PptxTree pptxTree = PptxTree();
+      String title = 'test title';
+
+      pptxTree = pptxTree.rebuild((b) => b..title = title);
+
+      expect(pptxTree.title!, title);
+    });
+
+    test('author is initialized with null value', () async {
+      PptxTree pptxTree = PptxTree();
+
+      expect(pptxTree.author, null);
+    });
+
+    test('author can be updated with a String value', () async {
+      PptxTree pptxTree = PptxTree();
+      String author = 'test author';
+
+      pptxTree = pptxTree.rebuild((b) => b..author = author);
+
+      expect(pptxTree.author!, author);
     });
   });
 }
