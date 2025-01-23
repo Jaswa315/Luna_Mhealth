d0624e4959b5df2fbc60e662b13e5dc68729cfc5
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-22 22:08:14 -0800

    tests for shape type enum

---
+++ luna_authoring_system/test/pptx_data_objects/shape_type_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:luna_authoring_system/pptx_data_objects/shape_type.dart';
+
+void main() {
+  group('Tests for ShapeType', () {
+    test('ShapeType.values returns ShapeTypes that we want in luna.', () async {
+      expect(ShapeType.values.toSet(),
+          {ShapeType.picture, ShapeType.connection, ShapeType.textbox});
+    });
+
+    test('ShapeType.name matches the expected value.', () async {
+      expect(ShapeType.picture.name, 'picture');
+      expect(ShapeType.connection.name, 'connection');
+      expect(ShapeType.textbox.name, 'textbox');
+    });
+  });
+}
