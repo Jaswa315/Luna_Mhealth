034039573ceeef5ce8e67b0820b85a2e1e9551bb
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-22 21:45:52 -0800

    refactor pptxParser

--- luna_authoring_system/lib/pptx_tree_compiler/pptx_parser.dart
+++ luna_authoring_system/lib/pptx_tree_compiler/pptx_parser.dart
@@ -8,35 +8,30 @@ import 'package:luna_core/utils/types.dart';
 /// It will only parse the required info to form a luna module.
 class PptxParser {
   late PptxLoader _pptxLoader;
+  PptxTree _pptxTree = PptxTree();
 
   PptxParser(String pptxFilePath) {
     _pptxLoader = PptxLoader(pptxFilePath);
   }
 
-  PptxTree _updateTitleAndAuthor(PptxTree pptxTree) {
+  void _updateTitleAndAuthor() {
     Json coreMap = _pptxLoader.getJsonFromPptx("docProps/core.xml");
-    pptxTree.title = coreMap['cp:coreProperties']['dc:title'];
-    pptxTree.author = coreMap['cp:coreProperties']['dc:creator'];
-
-    return pptxTree;
+    _pptxTree.title = coreMap['cp:coreProperties']['dc:title'];
+    _pptxTree.author = coreMap['cp:coreProperties']['dc:creator'];
   }
 
-  PptxTree _updateWidthAndHeight(PptxTree pptxTree) {
+  void _updateWidthAndHeight() {
     Json presentationMap = _pptxLoader.getJsonFromPptx("ppt/presentation.xml");
-    pptxTree.width =
+    _pptxTree.width =
         EMU(int.parse(presentationMap['p:presentation']['p:sldSz']['_cx']));
-    pptxTree.height =
+    _pptxTree.height =
         EMU(int.parse(presentationMap['p:presentation']['p:sldSz']['_cy']));
-
-    return pptxTree;
   }
 
   PptxTree getPptxTree() {
-    PptxTree pptxTree = PptxTree();
+    _updateTitleAndAuthor();
+    _updateWidthAndHeight();
 
-    pptxTree = _updateTitleAndAuthor(pptxTree);
-    pptxTree = _updateWidthAndHeight(pptxTree);
-
-    return pptxTree;
+    return _pptxTree;
   }
 }
