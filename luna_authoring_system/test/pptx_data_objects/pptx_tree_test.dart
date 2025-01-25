d88e6aa99e68d6e8857560c2cea0657a1871aa42
Andrew Nah <auspiciouswho47@gmail.com>
2025-01-24 21:28:05 -0800

    pptx tree update

--- luna_authoring_system/test/pptx_data_objects/pptx_tree_test.dart
+++ luna_authoring_system/test/pptx_data_objects/pptx_tree_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
+import 'package:luna_authoring_system/pptx_data_objects/slide.dart';
 import 'package:luna_core/utils/emu.dart';
 
 void main() {
@@ -38,6 +39,26 @@ void main() {
       pptxTree.height = height;
 
       expect(pptxTree.height, height);
+    });
+
+    test('An error is thrown when slides is accessed before initialization.', () async {
+      expect(
+        () => PptxTree().slides,
+        throwsA(isA<Error>()),
+      );
+    });
+
+    test('slides can be updated with a list of slides', () async {
+      PptxTree pptxTree = PptxTree();
+
+      List<Slide> slides = [];
+      for (int i = 0; i < 3; i++) {
+        slides.add(Slide());
+      }
+
+      pptxTree.slides = slides;
+
+      expect(pptxTree.slides, slides);
     });
   });
 }
