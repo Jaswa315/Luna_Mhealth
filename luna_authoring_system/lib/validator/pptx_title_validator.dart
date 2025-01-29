780a8b20e1ff3e8d7230e0d103d581a54892b993
Alan Lai <alanlai@alans-mbp-4.lan>
2025-01-29 10:42:34 -0800

    [Luna Authoring + Core] Rename to PptxTitleValidator and move Validator interface to core

--- luna_authoring_system/lib/validator/pptx_title_validator.dart
+++ luna_authoring_system/lib/validator/pptx_title_validator.dart
@@ -1,17 +1,17 @@
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_authoring_system/validator/exception/authoring_system_data_tree_validation_exception.dart';
-import 'package:luna_authoring_system/validator/interface/i_data_tree_validator.dart';
+import 'package:luna_core/models/interfaces/validator.dart';
 
 /// A validator that checks the validity of the title of a `PptxTree`.
 ///
-/// This class implements the `IDataTreeValidator` and ensures that the
+/// This class implements the `IValidator` and ensures that the
 /// title of the associated `PptxTree` is not empty. If the title is empty,
 /// it throws an `AuthoringSystemDataTreeValidationException` with a specific message.
-class DataTreeModuleTitleValidator implements IDataTreeValidator {
+class PptxTitleValidator implements IValidator {
     final PptxTree _dataTree;
 
-    /// Constructs a `DataTreeModuleTitleValidator` with a [PptxTree].
-    DataTreeModuleTitleValidator(this._dataTree);
+    /// Constructs a `PptxTitleValidator` with a [PptxTree].
+    PptxTitleValidator(this._dataTree);
 
     /// Validates the title of the `_dataTree`.
     ///
--- luna_authoring_system/test/pptx_tree_validator_test.dart
+++ luna_authoring_system/test/pptx_tree_validator_test.dart
@@ -1,18 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:luna_authoring_system/validator/interface/i_data_tree_validator.dart';
-import 'package:luna_authoring_system/validator/data_tree_module_title_validator.dart';
+import 'package:luna_core/models/interfaces/validator.dart';
+import 'package:luna_authoring_system/validator/pptx_title_validator.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_authoring_system/validator/exception/authoring_system_data_tree_validation_exception.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Data Tree Validator Tests', () {
-    test('Data Tree Module Title cannot be empty', () {
+  group('Pptx Tree Validator Tests', () {
+    test('Pptx Tree Module Title cannot be empty', () {
       PptxTree pptxTree =
           PptxTree();
       pptxTree.title = "";
-      IDataTreeValidator validator = DataTreeModuleTitleValidator(pptxTree);
+      IValidator validator = PptxTitleValidator(pptxTree);
 
       expect(() => validator.validate(),
           throwsA(isA<AuthoringSystemDataTreeValidationException>()));
