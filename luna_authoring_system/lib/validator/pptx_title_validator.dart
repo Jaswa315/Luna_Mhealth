cc5e97c026d11a942cda4a04ecc19280586dcd6a
Alan Lai <alanlai@alans-mbp-4.lan>
2025-01-29 21:30:58 -0800

    Addition of luna constants, pptx length tests additions

--- luna_authoring_system/lib/validator/pptx_title_validator.dart
+++ luna_authoring_system/lib/validator/pptx_title_validator.dart
@@ -1,4 +1,5 @@
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
+import 'package:luna_core/luna_constants.dart';
 import 'package:luna_core/validator/i_validator.dart';
 import 'package:luna_core/validator/validator_error.dart';
 import 'package:luna_core/validator/validator_error_type.dart';
@@ -15,6 +16,9 @@ class PptxTitleValidator implements IValidator {
     Set<ValidatorError> errors = {};
     if (_pptxTree.title.trim().isEmpty) {
        errors.add(ValidatorError(ValidatorErrorType.pptxTitleHasNoVisibleCharacters));
+    }
+    if (_pptxTree.title.length > LunaConstants.maximumPptxTitleLength) {
+       errors.add(ValidatorError(ValidatorErrorType.pptxTitleIsTooLong));
     }
 
     return errors;
--- luna_authoring_system/test/validator/pptx_title_validator_test.dart
+++ luna_authoring_system/test/validator/pptx_title_validator_test.dart
@@ -4,6 +4,7 @@ import 'package:luna_authoring_system/validator/pptx_title_validator.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_core/validator/validator_error_type.dart';
 import 'package:luna_core/validator/validator_error.dart';
+import 'package:luna_core/luna_constants.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -28,5 +29,27 @@ void main() {
 
     expect(errors.length, 1);
     expect(errors.first.errorType, ValidatorErrorType.pptxTitleHasNoVisibleCharacters);
+  });
+
+    test('Pptx Title cannot be too long', () {
+    PptxTree pptxTree = PptxTree();
+    // build a string one more character larger than the maximum title length allowed
+    pptxTree.title  = 'a' * (LunaConstants.maximumPptxTitleLength + 1); 
+    IValidator validator = PptxTitleValidator(pptxTree);
+
+    Set<ValidatorError> errors = validator.validate();
+
+    expect(errors.length, 1);
+    expect(errors.first.errorType, ValidatorErrorType.pptxTitleIsTooLong);
+  });
+
+  test('Pptx Title is just short enough', () {
+    PptxTree pptxTree = PptxTree();
+    pptxTree.title  = 'a' * (LunaConstants.maximumPptxTitleLength); 
+    IValidator validator = PptxTitleValidator(pptxTree);
+
+    Set<ValidatorError> errors = validator.validate();
+
+    expect(errors.isEmpty, true);
   });
 }
