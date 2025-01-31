660f184b5bb91179b90b311a73bbafff1cf56e39
Victor Long <nackor91@gmail.com>
2025-01-31 04:06:10 -0800

    more tests, add package_info plus

--- luna_core/test/utils/versioning_test.dart
+++ luna_core/test/utils/versioning_test.dart
@@ -6,11 +6,29 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import 'package:luna_core/utils/versioning.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
 
   group('VersionManager Tests', () {
+
+    test('VersionManager - Test Singleton', () {
+      VersionManager manager1 = VersionManager();
+      VersionManager manager2 = VersionManager();
+
+      expect(identical(manager1, manager2));
+
+    });
+
+    test('VersionManager - Test Version Equivalence', () async {
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+      VersionManager manager = VersionManager();
+
+      expect(packageInfo.version == manager.version);
+
+    });
 
 
   });
