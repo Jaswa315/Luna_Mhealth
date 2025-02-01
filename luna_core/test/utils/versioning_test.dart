cb9ba91e576e43900a3c594494a9555c11433a60
Victor Long <nackor91@gmail.com>
2025-01-31 16:42:39 -0800

    got mock package info plus working

--- luna_core/test/utils/versioning_test.dart
+++ luna_core/test/utils/versioning_test.dart
@@ -7,10 +7,42 @@
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import 'package:luna_core/utils/versioning.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
+
+  TestWidgetsFlutterBinding.ensureInitialized();  
+  //platform channels are not available in unit tests
+  //taken from package_info_plus github (https://github.com/fluttercommunity/plus_plugins/blob/main/packages/package_info_plus/package_info_plus/test/package_info_test.dart)
+  const channel = MethodChannel('dev.fluttercommunity.plus/package_info');
+  final log = <MethodCall>[];
+
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    channel,
+    (MethodCall methodCall) async {
+      log.add(methodCall);
+      switch (methodCall.method) {
+        case 'getAll':
+          return <String, dynamic>{
+            'appName': 'Luna',
+            'buildNumber': '1',
+            'packageName': 'luna.core',
+            'version': '1.0',
+            'installerStore': null,
+          };
+        default:
+          assert(false);
+          return null;
+      }
+    },
+  );
+
+  tearDown(() {
+    log.clear();
+  });
 
   group('VersionManager Tests', () {
 
@@ -18,7 +50,7 @@ void main() {
       VersionManager manager1 = VersionManager();
       VersionManager manager2 = VersionManager();
 
-      expect(identical(manager1, manager2));
+      expect(identical(manager1, manager2), true);
 
     });
 
@@ -26,9 +58,9 @@ void main() {
       PackageInfo packageInfo = await PackageInfo.fromPlatform();
       VersionManager manager = VersionManager();
 
-      expect(packageInfo.version == manager.version);
+      expect((packageInfo.version == manager.version), true);
 
-    });
+  });
 
 
   });
