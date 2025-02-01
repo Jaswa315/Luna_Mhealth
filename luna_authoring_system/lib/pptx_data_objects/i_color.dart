17fbec18bd44ecf8feca9874ef851e37003cb325
Andrew Nah <auspiciouswho47@gmail.com>
2025-02-01 15:23:46 -0800

    test for i color

--- luna_authoring_system/lib/pptx_data_objects/i_color.dart
+++ luna_authoring_system/lib/pptx_data_objects/i_color.dart
@@ -4,8 +4,7 @@ import 'dart:ui' show Color;
 /// This abstract IColor class allows concrete color classes to have
 /// their own way to transform their color representation to dart color object.
 abstract class IColor {
-  Color get color;
-  set color(Color color);
+  late Color color;
 
   Color toColor();
 }
