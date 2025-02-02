da0a828282893b58d4152b99dec99c027521b2fb
Victor Long <nackor91@gmail.com>
2025-02-02 02:22:24 -0800

    add relationship file, and list to slides

--- luna_authoring_system/lib/pptx_data_objects/slide.dart
+++ luna_authoring_system/lib/pptx_data_objects/slide.dart
@@ -1,8 +1,15 @@
+import 'package:luna_authoring_system/pptx_data_objects/relationship.dart';
 import 'package:luna_authoring_system/pptx_data_objects/shape.dart';
 
 /// The slide represents the slide data in PowerPoint.
 class Slide {
   late List<Shape>? shapes;
+
+  late List<Relationship>? relationships;
+
+  /// fileName of the slide
+  /// Needed to form relationships
+  late String fileName;
 
   Slide();
 }
