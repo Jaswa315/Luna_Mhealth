de1603c2258dd6684d8dab158c7fe25b015587c5
Andrew Nah <auspiciouswho47@gmail.com>
2025-02-03 00:16:12 -0800

    update main into flutter arguments

--- luna_authoring_system/lib/main.dart
+++ luna_authoring_system/lib/main.dart
@@ -1,25 +1,40 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'package:luna_authoring_system/module_object_generator.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_authoring_system/pptx_tree_compiler/pptx_parser.dart';
 import 'package:luna_core/models/module.dart';
 import 'package:luna_core/storage/module_resource_factory.dart';
+import 'package:luna_core/utils/logging.dart';
 import 'package:path/path.dart' as p;
 
-Future<void> main(List<String> args) async {
-  if (args.length != 3) {
+Future<void> main() async {
+  // initialize log manager
+  WidgetsFlutterBinding.ensureInitialized();
+  await GlobalConfiguration().loadFromAsset("app_settings");
+  await LogManager.createInstance();
+
+  const String pptxFilePath = String.fromEnvironment('pptxFilePath');
+  const String outputDir = String.fromEnvironment('outputDir');
+  const String moduleName = String.fromEnvironment('moduleName');
+
+  // ignore: unnecessary_null_comparison
+  if (pptxFilePath == null || outputDir == null || moduleName == null) {
     // ignore: avoid_print
-    print('Usage: dart main.dart <pptx_file_path> <output_dir> <module_name>');
+    // You'll need to change this or add the sample module to autoload the module.
+    // Files are under /Users/username/Library/Containers/luna_authoring_system/Data by default on Macos
+    // On Windows, Files are generated under C:\Users\username\Documents.
+    // data/sample for temp assets data/documents for the module.luna file
+    print(
+      'Usage: flutter run --dart--define=pptxFilePath=<pptx_file_path> --dart-define=outputDir=<output_dir> --dart-define=moduleName=<module_name>',
+    );
 
     // Exit with code -1 to indicate an error
     exit(-1);
   }
-
-  String pptxFilePath = args[0];
-  String outputDir = args[1];
-  String moduleName = args[2];
 
   // Parse the presentation
   PptxParser pptxParser = PptxParser(pptxFilePath);
