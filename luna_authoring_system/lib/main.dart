00451ad1b871285f9219814cfeac87b18b3fe63d
Alan Lai <alanlai@alans-mbp-4.lan>
2025-02-04 19:08:24 -0800

    main lint ignore

--- luna_authoring_system/lib/main.dart
+++ luna_authoring_system/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:global_configuration/global_configuration.dart';
 import 'package:luna_authoring_system/module_object_generator.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_authoring_system/pptx_tree_compiler/pptx_parser.dart';
-import 'package:luna_authoring_system/validator/pptx_validations.dart';
+import 'package:luna_authoring_system/validator/pptx_validator.dart';
 import 'package:luna_core/models/module.dart';
 import 'package:luna_core/storage/module_resource_factory.dart';
 import 'package:luna_core/utils/logging.dart';
