cd33e21016e7decd77822991bb91947b6ed63e37
alanlai1738 <alan.lai1738@gmail.com>
2025-02-11 16:18:21 -0800

    Delete Point2D Validator

--- luna_core/test/models/point/point2d_percentage_test.dart
+++ luna_core/test/models/point/point2d_percentage_test.dart
@@ -21,46 +21,6 @@ void main() {
       );
     });
 
-    // Ensures an error is thrown if x or y is negative.
-    test('Throws ArgumentError when x is negative', () {
-      expect(
-        () => Point2DPercentage(-0.5, 0.5),
-        throwsA(
-          allOf(
-            isA<ArgumentError>(),
-            predicate(
-                (e) => e.toString().contains('point2DXPercentageLessThanZero')),
-          ),
-        ),
-      );
-    });
-
-    test('Throws ArgumentError when y is negative', () {
-      expect(
-        () => Point2DPercentage(0.5, -0.5),
-        throwsA(
-          allOf(
-            isA<ArgumentError>(),
-            predicate(
-                (e) => e.toString().contains('point2DYPercentageLessThanZero')),
-          ),
-        ),
-      );
-    });
-
-    test('Throws ArgumentError when x is greater than 1', () {
-      expect(
-        () => Point2DPercentage(1.2, 0.5),
-        throwsA(
-          allOf(
-            isA<ArgumentError>(),
-            predicate((e) =>
-                e.toString().contains('point2DXPercentageGreaterThanOne')),
-          ),
-        ),
-      );
-    });
-
     // Ensures valid x and y values between 0 and 1 are accepted.
     test('Creates Point2DPercentage with valid values (0 to 1)', () {
       final point = Point2DPercentage(0.3, 0.7);
