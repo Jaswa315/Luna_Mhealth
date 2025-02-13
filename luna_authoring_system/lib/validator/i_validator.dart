5a848d05247eeb1616bade9784774ea33c3fddd1
alanlai1738 <alan.lai1738@gmail.com>
2025-02-12 19:39:36 -0800

    Added comments to I Validator

--- luna_authoring_system/lib/validator/i_validator.dart
+++ luna_authoring_system/lib/validator/i_validator.dart
@@ -1,3 +1,5 @@
+/// An abstract base class for all validators.
+/// Validators return a set of Validation Issues.
 import 'package:luna_authoring_system/validator/i_validation_issue.dart';
 
 abstract class IValidator {
