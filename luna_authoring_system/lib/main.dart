27954abc77b10062a91e20ed291c396492b1bb26
Victor Long <nackor91@gmail.com>
2025-02-27 20:39:34 -0800

    add basic gui

--- luna_authoring_system/lib/main.dart
+++ luna_authoring_system/lib/main.dart
@@ -1,16 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:luna_authoring_system/helper/authoring_initializer.dart';
 import 'package:luna_authoring_system/pptx_tree_compiler/pptx_runner.dart';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:luna_authoring_system/user_interface/authoring_home_screen.dart';
+
 
 
 Future<void> main(List<String> arguments) async {
   // Initialize AuthoringSystem singletons and load app settings
-  await AuthoringInitializer.initializeAuthoring();
+  //await AuthoringInitializer.initializeAuthoring();
 
+  var input_to_process = <String>[];
 
-  /// This is to ensure that the current --dart-define method works
-  /// will be folded into validateInputs once arguments work
-  final pptxFile = AuthoringInitializer.processInputs(arguments);
+  if (arguments.isEmpty){
+    // No Arguments present GUI
+    runApp(MyApp());
+    
+    return;
+    //final path = await FilePicker.platform.pickFiles();
+    //input_to_process.add(path!.paths[0]!);
+    //input_to_process.add("gui");
+  }else{
+    input_to_process = arguments;
+  }
+
+  final pptxFile = AuthoringInitializer.processInputs(input_to_process);
+  
 
   /// Process the pptxFile and output the Luna file
-  await PptxRunner().processPptx(pptxFile, arguments[1]);
+  await PptxRunner().processPptx(pptxFile, input_to_process[1]);
+  print("test");
+}
+
+/// The root widget of the application.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.microtask(() => AuthoringInitializer.initializeAuthoring());
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthoringHomeScreen(),
+    );
+  }
 }
---
+++ luna_authoring_system/lib/user_interface/authoring_home_screen.dart
@@ -0,0 +1,70 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+
+class AuthoringHomeScreen extends StatefulWidget {
+  @override
+  _AuthoringHomeScreenState createState() => _AuthoringHomeScreenState();
+}
+
+class _AuthoringHomeScreenState extends State<AuthoringHomeScreen> {
+  String? filePath;
+  bool filePicked = false;
+  bool textEntered = false;
+
+  final TextEditingController _controller = TextEditingController();
+
+  void pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        filePath = result.files.single.name;
+        filePicked = true;
+      });
+    }
+  }
+
+  void submitText() {
+    setState(() {
+      textEntered = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Luna Authoring System")),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (!filePicked) 
+                ElevatedButton(
+                  onPressed: pickFile, 
+                  child: Text("Pick a PPTX File"),
+                ),
+              if (filePicked && !textEntered) ...[
+                Text("File Selected: $filePath"),
+                TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(labelText: "Enter Module Name"),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: submitText,
+                  child: Text("Submit"),
+                ),
+              ],
+              if (textEntered) Text("Job Done!", style: TextStyle(fontSize: 20)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
