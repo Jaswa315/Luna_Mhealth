d8d08a5232801a1db212aa589dc88ef92727be4d
Jaswanth Lagdapati <jaswanthsrivan.315@gmail.com>
2025-03-10 15:13:41 -0700

    Refactoring Module Constructor.

---
+++ luna_authoring_system/lib/builder/module_constructor.dart
@@ -0,0 +1,21 @@
+import 'package:luna_authoring_system/builder/module_builder.dart';
+import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
+import 'package:luna_core/models/module.dart';
+
+class ModuleConstructor {
+  final PptxTree pptxTree;
+
+  ModuleConstructor(this.pptxTree);
+
+  Future<Module> constructLunaModule() async {
+    final moduleWidth = pptxTree.width.value.toInt();
+    final moduleHeight = pptxTree.height.value.toInt();
+
+    return ModuleBuilder()
+        .setTitle(pptxTree.title)
+        .setAuthor(pptxTree.author)
+        .setDimensions(moduleWidth, moduleHeight)
+        .setPages(pptxTree)
+        .build();
+  }
+}
