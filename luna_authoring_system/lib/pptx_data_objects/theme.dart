93f8e1bae927267008b82c0273c7a0e3e47fbc2f
Jaswanth Lagdapati <jaswanthsrivan.315@gmail.com>
2025-03-12 15:33:27 -0700

    Making the thicknessMap private Also adding some unit tests to properly test it.

--- luna_authoring_system/lib/pptx_data_objects/theme.dart
+++ luna_authoring_system/lib/pptx_data_objects/theme.dart
@@ -2,17 +2,26 @@ import 'package:luna_core/utils/emu.dart';
 
 class Theme {
   final int themeId;
-  late final Map<int, EMU> thicknessMap;
+  final Map<int, EMU> _thicknessMap;
 
-  Theme(this.themeId) {
-    thicknessMap = {};
-  }
+  Theme(this.themeId) : _thicknessMap = {};
 
-  void addThickness(int key, EMU value) {
-    thicknessMap[key] = value;
+  void setThickness(int key, EMU value) {
+    if (value.value <= 0) {
+      throw ArgumentError('Thickness value must be greater than 0');
+    }
+    _thicknessMap[key] = value;
   }
 
   EMU getThickness(int key) {
-    return thicknessMap[key] ?? EMU(0);
+    return _thicknessMap[key] ?? EMU(0);
+  }
+
+  bool hasThickness(int key) {
+    return _thicknessMap.containsKey(key);
+  }
+
+  void removeThickness(int key) {
+    _thicknessMap.remove(key);
   }
 }
