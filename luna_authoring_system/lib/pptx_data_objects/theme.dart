55b295a3da4eef6ede50a784f5a8909fecb33aa3
Jaswanth Lagdapati <jaswanthsrivan.315@gmail.com>
2025-03-14 10:39:19 -0700

    Made the theme id private.

--- luna_authoring_system/lib/pptx_data_objects/theme.dart
+++ luna_authoring_system/lib/pptx_data_objects/theme.dart
@@ -1,10 +1,12 @@
 import 'package:luna_core/utils/emu.dart';
 
 class Theme {
-  final int themeId;
+  final int _themeId;
   final Map<int, EMU> _thicknessMap;
 
-  Theme(this.themeId) : _thicknessMap = {};
+  Theme(this._themeId) : _thicknessMap = {};
+
+  int get themeId => _themeId;
 
   void setThickness(int key, EMU thickness) {
     if (thickness.value <= 0) {
