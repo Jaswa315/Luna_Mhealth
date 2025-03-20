5d564922b04454b73f2a1e6a9a2c1834f3624062
MikeyLee133 <leemichael0000@gmail.com>
2025-03-20 11:18:38 -0700

    changing the name of pptx_parser to pptx_tree_builder

--- luna_authoring_system/test/pptx_tree_compiler/pptx_parser_test.dart
+++ luna_authoring_system/test/pptx_tree_compiler/pptx_parser_test.dart
@@ -1,4 +1,4 @@
-import 'package:luna_authoring_system/pptx_tree_compiler/pptx_parser.dart';
+import 'package:luna_authoring_system/pptx_tree_compiler/pptx_tree_builder.dart';
 import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
 import 'package:luna_authoring_system/pptx_data_objects/slide.dart';
 import 'package:luna_authoring_system/pptx_data_objects/connection_shape.dart';
@@ -9,44 +9,43 @@ import 'package:luna_core/utils/emu.dart';
 import 'dart:io';
 
 void main() {
-  group('Tests for PptxParser using A line.pptx', () {
-
+  group('Tests for PptxTreeBuilder using A line.pptx', () {
     final pptxFile = File('test/test_assets/A line.pptx');
-    PptxParser pptxParser = PptxParser(pptxFile);
+    PptxTreeBuilder pptxTreeBuilder = PptxTreeBuilder(pptxFile);
 
     test('parsePptx method initialzes title.', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
       expect(pptxTree.title, "A line");
     });
 
     test('parsePptx method initialzes author.', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
 
       expect(pptxTree.author, "Andrew Nah");
     });
 
     test('parsePptx method initialzes width.', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
 
       expect(pptxTree.width, isA<EMU>());
       expect(pptxTree.width.value, 12192000);
     });
 
     test('parsePptx method initialzes height.', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
 
       expect(pptxTree.height, isA<EMU>());
       expect(pptxTree.height.value, 6858000);
     });
 
     test('parsePptx method initialzes slides.', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
 
       expect(pptxTree.slides, isA<List<Slide>>());
     });
 
     test('A line is parsed', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
       ConnectionShape cShape = pptxTree.slides[0].shapes![0] as ConnectionShape;
 
       expect(cShape.type, ShapeType.connection);
@@ -59,7 +58,7 @@ void main() {
 
     ///testing in a pptx file where flipV = 1
     test('flipV attribute is correctly parsed for connection shapes', () async {
-      PptxTree pptxTree = pptxParser.getPptxTree();
+      PptxTree pptxTree = pptxTreeBuilder.getPptxTree();
       ConnectionShape cShape = pptxTree.slides[0].shapes![0] as ConnectionShape;
 
       expect(cShape.isFlippedVertically, isTrue);
