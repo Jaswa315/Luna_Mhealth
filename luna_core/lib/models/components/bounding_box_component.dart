03ce452f90ce2585a92db827d1c5aee515fbdd52
Jaswanth Lagdapati <jaswanthsrivan.315@gmail.com>
2025-04-11 09:27:44 -0700

    Refactoring the bounding_box_component

--- luna_core/lib/models/components/bounding_box_component.dart
+++ luna_core/lib/models/components/bounding_box_component.dart
@@ -1,25 +1,26 @@
 import 'package:luna_core/models/bounding_box.dart';
+import 'package:luna_core/utils/types.dart';
 
-/*  BBComponent represents a component with a bounding box, such as a text box or image.
-    It is used to define spatial layout for components that require specific positioning and sizing. */
-class BBComponent {
+///BBComponent represents a component with a bounding box, such as a text box or image.
+///It is used to define spatial layout for components that require specific positioning and sizing. */
+class BoundingBoxComponent {
   /// The bounding box specifying the start position, width, and height of the component.
   final BoundingBox boundingBox;
 
   /// Creates a BBComponent with a required bounding box.
-  BBComponent({required this.boundingBox});
+  BoundingBoxComponent({required this.boundingBox});
 
   /// Factory method to create a BBComponent instance from a JSON map.
-  factory BBComponent.fromJson(Map<String, dynamic> json) {
-    return BBComponent(
+  factory BoundingBoxComponent.fromJson(Map<String, dynamic> json) {
+    return BoundingBoxComponent(
       boundingBox: BoundingBox.fromJson(json['boundingBox']),
     );
   }
 
   /// Converts the BBComponent instance to a JSON map.
-  Map<String, dynamic> toJson() {
+  Json toJson() {
     return {
-      'type': 'bbComponent',
+      'type': 'boundingBoxComponent',
       'boundingBox': boundingBox.toJson(),
     };
   }
--- luna_core/test/models/components/bounding_box_component_test.dart
+++ luna_core/test/models/components/bounding_box_component_test.dart
@@ -1,20 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:luna_core/models/components/bb_component.dart';
+import 'package:luna_core/models/components/bounding_box_component.dart';
 import 'package:luna_core/models/bounding_box.dart';
 import 'package:luna_core/utils/emu.dart';
 import 'package:luna_core/utils/display_pixel.dart';
 import 'dart:ui';
 
 void main() {
-  group('BBComponent', () {
-    test('should create BBComponent with valid bounding box', () {
+  group('BoundingBoxComponent', () {
+    test('should create BoundingBoxComponent with valid bounding box', () {
       final boundingBox = BoundingBox(
         topLeftCorner: Offset(10.0, 20.0),
         width: EMU(5000),
         height: DisplayPixel(100.0),
       );
 
-      final component = BBComponent(boundingBox: boundingBox);
+      final component = BoundingBoxComponent(boundingBox: boundingBox);
 
       expect(component.boundingBox.topLeftCorner.dx, 10.0);
       expect(component.boundingBox.topLeftCorner.dy, 20.0);
@@ -22,17 +22,17 @@ void main() {
       expect((component.boundingBox.height as DisplayPixel).value, 100.0);
     });
 
-    test('should serialize BBComponent to JSON correctly', () {
+    test('should serialize BoundingBoxComponent to JSON correctly', () {
       final boundingBox = BoundingBox(
         topLeftCorner: Offset(5.0, 15.0),
         width: EMU(3000),
         height: DisplayPixel(75.0),
       );
 
-      final component = BBComponent(boundingBox: boundingBox);
+      final component = BoundingBoxComponent(boundingBox: boundingBox);
       final json = component.toJson();
 
-      expect(json['type'], 'bbComponent');
+      expect(json['type'], 'boundingBoxComponent');
       expect(json['boundingBox']['topLeftCorner']['dx'], 5.0);
       expect(json['boundingBox']['topLeftCorner']['dy'], 15.0);
       expect(json['boundingBox']['width']['unit'], 'emu');
@@ -41,9 +41,9 @@ void main() {
       expect(json['boundingBox']['height']['value'], 75.0);
     });
 
-    test('should deserialize BBComponent from JSON correctly', () {
+    test('should deserialize BoundingBoxComponent from JSON correctly', () {
       final json = {
-        'type': 'bbComponent',
+        'type': 'boundingBoxComponent',
         'boundingBox': {
           'topLeftCorner': {'dx': 3.0, 'dy': 6.0},
           'width': {'unit': 'emu', 'value': 4000},
@@ -51,7 +51,7 @@ void main() {
         }
       };
 
-      final component = BBComponent.fromJson(json);
+      final component = BoundingBoxComponent.fromJson(json);
 
       expect(component.boundingBox.topLeftCorner.dx, 3.0);
       expect(component.boundingBox.topLeftCorner.dy, 6.0);
@@ -66,7 +66,7 @@ void main() {
         height: DisplayPixel(0),
       );
 
-      final component = BBComponent(boundingBox: boundingBox);
+      final component = BoundingBoxComponent(boundingBox: boundingBox);
 
       expect(component.boundingBox.topLeftCorner.dx, 0.0);
       expect(component.boundingBox.topLeftCorner.dy, 0.0);
