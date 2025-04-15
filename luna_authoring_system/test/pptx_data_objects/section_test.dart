9e822ad1beec51f9c1467fcb47ec2ab0fa7a10ab
Andrew Nah <auspiciouswho47@gmail.com>
2025-04-14 21:55:14 -0700

    0,1, n rule to unit tests

--- luna_authoring_system/test/pptx_data_objects/section_test.dart
+++ luna_authoring_system/test/pptx_data_objects/section_test.dart
@@ -6,17 +6,24 @@ void main() {
     test('Section object stores valid value.', () {
       Section section = Section({
         'Symptom1': [1, 2, 3],
-        'Symptom2': [],
+        'Symptom2': [4],
+        'Symptom3': [],
       });
 
       expect(section.value['Symptom1'], [1, 2, 3]);
-      expect(section.value['Symptom2'], []);
+      expect(section.value['Symptom2'], [4]);
+      expect(section.value['Symptom3'], []);
     });
 
     test('Invalid Section values should throw an ArgumentError.', () {
       expect(
           () => Section({
                 'Symptom1': [-1]
+              }),
+          throwsArgumentError);
+      expect(
+          () => Section({
+                'Symptom1': [1, 2, -2]
               }),
           throwsArgumentError);
     });
