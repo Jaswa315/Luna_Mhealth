fcc21c6a580ee67408e779aa8d129058f5ccc89b
Andrew Nah <auspiciouswho47@gmail.com>
2025-04-17 17:12:04 -0700

    fix unit test names

--- luna_authoring_system/test/pptx_data_objects/simple_type_percentage_test.dart
+++ luna_authoring_system/test/pptx_data_objects/simple_type_percentage_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:luna_authoring_system/pptx_data_objects/simple_type_percentage.dart';
 
 void main() {
-  group('Alpha Class Tests', () {
-    test('Alpha object stores valid value.', () {
+  group('SimpleTypePercentage Class Tests', () {
+    test('SimpleTypePercentage object stores valid value.', () {
       SimpleTypePercentage STPercentage1 = SimpleTypePercentage(0);
       SimpleTypePercentage STPercentage2 = SimpleTypePercentage(50000);
       SimpleTypePercentage STPercentage3 = SimpleTypePercentage(100000);
@@ -13,18 +13,18 @@ void main() {
       expect(STPercentage3.value, 100000);
     });
 
-    test('Valid Alpha values should not throw an error.', () {
+    test('Valid SimpleTypePercentage values should not throw an error.', () {
       expect(() => SimpleTypePercentage(0), returnsNormally);
       expect(() => SimpleTypePercentage(50000), returnsNormally);
       expect(() => SimpleTypePercentage(100000), returnsNormally);
     });
 
-    test('Invalid Alpha values should throw an ArgumentError.', () {
+    test('Invalid SimpleTypePercentage values should throw an ArgumentError.', () {
       expect(() => SimpleTypePercentage(-1), throwsArgumentError);
       expect(() => SimpleTypePercentage(100001), throwsArgumentError);
     });
 
-    test('Edge cases for Alpha values returns Normally.', () {
+    test('Edge cases for SimpleTypePercentage values returns Normally.', () {
       expect(
           () => SimpleTypePercentage(
               SimpleTypePercentage.minSimpleTypePercentage),
