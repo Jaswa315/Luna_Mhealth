8259b1fd9fcb3049b1a5b4e389154886ab3198b2
Kameron Vuong <vuongkam@uw.edu>
2025-04-18 13:01:02 -0700

    parse odp title in meta.xml

---
+++ luna_authoring_system/test/odp_tree_compiler/odp_tree_builder_test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:luna_authoring_system/odp_tree_compiler/odp_tree_builder.dart';
+import 'package:luna_authoring_system/pptx_data_objects/pptx_tree.dart';
+
+void main() {
+  group('Tests for OdpTreeBuilder using A horizontal line.odp', () {
+    final odpFile = File('test/test_assets/A horizontal line.odp');
+    OdpTreeBuilder odpTreeBuilder = OdpTreeBuilder(odpFile);
+    PptxTree odpTree = odpTreeBuilder.getOdpTree();
+    test('parseOdp method initialzes title.', () async {
+      expect(odpTree.title, "A horizontal line");
+    });
+  });
+}
