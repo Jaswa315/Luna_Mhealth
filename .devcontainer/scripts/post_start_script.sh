49345ff47f0ba4d419480c7e4ff3e3c0726f31bf
Victor Long <vlong91@uw.edu>
2025-04-20 19:25:48 +0000

    Merged PR 1326: Fix vncserver startup issue
    # Context
    When starting the dev container, the vncserver is not starting. This was due to a logging change that I made that I spose I didn't test before my last PR.
    
    I fixed this by removing nohup and just running vncserver in the background.
    
    Since this just a change to the startup script, it will take affect on container restart and does not require a rebuild.

--- .devcontainer/scripts/post_start_script.sh
+++ .devcontainer/scripts/post_start_script.sh
@@ -4,14 +4,18 @@
 #run in the background
 # https://code.visualstudio.com/remote/advancedcontainers/start-processes
 echo "Starting VNC server..." 
-nohup bash -c "vncserver :1 -geometry 1280x720 \
- -depth 16 -SecurityTypes None" > /var/log/vncserver.log &
+vncserver :1 -geometry 1280x720 \
+ -depth 16 -SecurityTypes None > /var/log/vncserver.log
+
+sleep 1
 echo "Starting noVNC server..." 
 nohup bash -c "/opt/noVNC/utils/websockify/run \
   --web=/opt/noVNC \
   6080 \
-  localhost:5901" \
-  > /var/log/websockify.log &
+  localhost:5901"\
+  > /var/log/websockify.log 2>&1 &
 
 sleep 1
+
 echo "#### Setup done, you can close this terminal. ######"
+
