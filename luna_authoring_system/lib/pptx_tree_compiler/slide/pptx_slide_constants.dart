18ec57d5d9c70bddbc13866721c6958e3cb983ef
Andrew Nah <auspiciouswho47@gmail.com>
2025-04-23 20:52:39 -0700

    delete unused constant

--- luna_authoring_system/lib/pptx_tree_compiler/slide/pptx_slide_constants.dart
+++ luna_authoring_system/lib/pptx_tree_compiler/slide/pptx_slide_constants.dart
@@ -3,9 +3,6 @@
 /// =================================================================================================
 /// This file contains the XML element constants used in the PptxSlideBuilder class.
 ///
-/// The XML element for the connection shape element in a PowerPoint presentation.
-const String eConnectionShape = 'p:cxnSp';
-
 /// The XML element for the common slide data in the slide.
 const String eCommonSlideData = 'p:cSld';
 
