30693b8b4831ac5b91c7ca83a56e6ab115a101b7
Andrew Nah <auspiciouswho47@gmail.com>
2025-04-24 12:36:59 -0700

    add class annotation

--- luna_authoring_system/lib/pptx_tree_compiler/transform/pptx_transform_builder.dart
+++ luna_authoring_system/lib/pptx_tree_compiler/transform/pptx_transform_builder.dart
@@ -4,6 +4,7 @@ import 'package:luna_core/units/emu.dart';
 import 'package:luna_core/units/point.dart';
 import 'package:luna_core/utils/types.dart';
 
+/// This class is responsible for building the Transform object from a JSON map.
 class PptxTransformBuilder {
   Transform getTransform(Json transformMap) {
     Point offset = Point(
