4262ab0b6c29af9aeac7334d0809f26b3af14fba
Jaswanth Lagdapati <jaswanthsrivan.315@gmail.com>
2025-05-14 16:11:50 -0700

    Adding comments to page_view.dart

--- luna_mobile/lib/presentation/widgets/page_view.dart
+++ luna_mobile/lib/presentation/widgets/page_view.dart
@@ -4,6 +4,11 @@ import 'package:luna_mobile/renderers/irenderer.dart';
 import 'package:luna_mobile/renderers/renderer_factory.dart';
 import 'package:luna_mobile/states/module_state.dart';
 
+/// A widget that displays the current page of a module.
+///
+/// This widget is part of the module rendering system and is driven by the
+/// [ModuleState], which manages the state and navigation of pages within a module.
+/// It renders all components of the current page using their respective renderers.
 class PageView extends StatelessWidget {
   final ModuleState moduleState;
 
@@ -16,6 +21,9 @@ class PageView extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     final page_model.Page currentPage = moduleState.getCurrentPage();
 
+    // Render a scrollable container that stacks all components of the current page.
+    // For each component, resolve its renderer via RendererFactory and render it
+    // with the screen size.
     return SingleChildScrollView(
       child: Container(
         width: screenSize.width,
