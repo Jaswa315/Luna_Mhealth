ad5a3930584e0655dcfeb92ef631977c3121bddc
MikeyLee133 <leemichael0000@gmail.com>
2025-05-14 13:24:57 -0700

    Creating PageNavigationController

--- luna_mobile/lib/controllers/page_navigation_controller.dart
+++ luna_mobile/lib/controllers/page_navigation_controller.dart
@@ -2,6 +2,17 @@ import 'package:luna_core/models/pages/page.dart';
 import 'package:luna_core/models/pages/sequence_of_pages.dart';
 import 'package:luna_mobile/states/module_state.dart';
 
+/// A controller class responsible for managing page navigation within a module.
+///
+/// This class interacts with the `ModuleState` to retrieve the current page
+/// and navigate between pages in a `SequenceOfPages`. It ensures that the
+/// application's state is updated consistently when navigating to the next
+/// or previous page in the sequence.
+///
+/// Responsibilities:
+/// - Retrieve the current page from the `ModuleState`.
+/// - Determine the next or previous page in the sequence.
+/// - Update the `ModuleState` with the new current page after navigation.
 class PageNavigationController {
   final ModuleState moduleState;
 
@@ -16,14 +27,12 @@ class PageNavigationController {
   }
 
   /// Navigates to the next page in the sequence, if available.
-  /// Returns `true` if navigation was successful, otherwise `false`.
   void navigateToNextPage() {
     final nextPage = currentSequence.getRightPage(currentPage);
     moduleState.setCurrentPage(nextPage);
   }
 
   /// Navigates to the previous page in the sequence, if available.
-  /// Returns `true` if navigation was successful, otherwise `false`.
   void navigateToPreviousPage() {
     final previousPage = currentSequence.getLeftPage(currentPage);
     moduleState.setCurrentPage(previousPage);
