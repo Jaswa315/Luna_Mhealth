de3a9f1fd1f0aca9fc8da62ce6c24c253501cdd1
sushmamanthena <sushmam@uw.edu>
2025-05-29 19:10:56 -0700

    Added class documentation and resolved comments

--- luna_authoring_system/lib/user_interface/validation_issues_summary.dart
+++ luna_authoring_system/lib/user_interface/validation_issues_summary.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:luna_authoring_system/validator/i_validation_issue.dart';
 import 'package:luna_authoring_system/validator/issue/pptx_issues/i_pptx_validation_issues.dart';
 
+/// This Widget [ValidationIssuesSummary] displays a summary of validation issues.
+/// It takes a list of [IValidationIssue] and displays them in a formatted manner.
 class ValidationIssuesSummary extends StatelessWidget {
-  final List<IValidationIssue> issues;
+  final List<IValidationIssue> _issues;
 
-  const ValidationIssuesSummary({Key? key, required this.issues})
-      : super(key: key);
+  const ValidationIssuesSummary(
+      {Key? key, required List<IValidationIssue> issues})
+      : _issues = issues,
+        super(key: key);
 
-  bool get hasIssues => issues.isNotEmpty;
+  bool get hasIssues => _issues.isNotEmpty;
 
   Widget _buildPptxValidationIssueRow(IPptxValidationIssues issue) {
     return Row(
@@ -51,14 +55,14 @@ class ValidationIssuesSummary extends StatelessWidget {
       children: [
         SizedBox(height: 20),
         Text(
-          "Validation Issues Found: ${issues.length}",
+          "Validation Issues Found: ${_issues.length}",
           style: const TextStyle(
             color: Colors.red,
             fontWeight: FontWeight.bold,
           ),
         ),
         SizedBox(height: 8),
-        ...issues.map((issue) => _buildIssueRow(issue)),
+        ..._issues.map((issue) => _buildIssueRow(issue)),
       ],
     );
   }
