ffabdcddedb064954e8db03871fc876af71ec817
sushmamanthena <sushmam@uw.edu>
2025-06-13 06:50:30 -0700

    Removed info and error from ValidationSeverity enum

--- luna_authoring_system/lib/luna_constants.dart
+++ luna_authoring_system/lib/luna_constants.dart
@@ -5,7 +5,5 @@ class LunaConstants {
 
 /// Severity levels enum for validation issues in Luna authoring system
 enum ValidationSeverity {
-  info, // Informational issues that do not require immediate attention
   warning, // Issues that should be addressed but do not block functionality
-  error, // Issues that block functionality and need to be fixed
 }
