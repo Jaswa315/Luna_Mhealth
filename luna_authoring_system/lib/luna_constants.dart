16e6b7465b0444ad8dcb4c5920b562d7fa335353
lakshmi0215 <kslakshmi0226@gmail.com>
2025-09-11 16:22:56 -0700

    keep CsvHeaders in sync with validator/service

--- luna_authoring_system/lib/luna_constants.dart
+++ luna_authoring_system/lib/luna_constants.dart
@@ -10,6 +10,4 @@ class CsvHeaders {
 }
 
 /// Severity levels enum for validation issues in Luna authoring system
-enum ValidationSeverity {
-  warning, // Issues that should be addressed but do not block functionality
-}
+enum ValidationSeverity { info, warning, error }
